4123937e406cf5875d89336f5a6b7179cf69dc4a
Your Name <advmenendez92@gmail.com>
2021-10-05 16:04:56 +0200

    Iteration#2

---
+++ Iteration2.js
@@ -0,0 +1,20 @@
+
+const avengers = ['Hulk', 'Thor', 'IronMan', 'Captain A.', 'Spiderman', 'Captain M.'];
+function findLongestWord(avengers) {
+      
+    let avengerBiggest = "";
+  
+    for (let index = 0; index < avengers.length; index++) {
+      console.log(avengers[index]);
+      if (avengers[index].length > avengerBiggest.length) {
+        avengerBiggest = avengers[index];
+      }
+      if (avengers[index].length === avengerBiggest.length) {
+        avengerBiggest = avengers[index];
+      }
+
+    }
+    console.log("Nombre más largo: ", avengerBiggest);
+  }
+  
+  findLongestWord(avengers);
